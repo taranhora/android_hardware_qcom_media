935397346e8ffa16341d2e005c1b6f66fbd1d1f5
Satya Krishna Pindiproli <satyak@codeaurora.org>
2017-10-05 17:53:07 +0530

    mm-core: move multiaac decoder component above dummy
    Multiaac decoder OMX component is being used by some customers
    so move it above the dummy component to expose it in the registry
    table for all relevant targets.
    
    CRs-Fixed: 2113889
    Change-Id: I5fb391fadfdd6675db7028ae861f2f974574bd4b

--- mm-core/src/msm8996/registry_table_android.c
+++ mm-core/src/msm8996/registry_table_android.c
@@ -669,6 +669,22 @@ omx_core_cb_type core[] =
     }
   },
   {
+    "OMX.qcom.audio.decoder.multiaac",
+    NULL,   // Create instance function
+    // Unique instance handle
+    {
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    },
+    NULL,   // Shared object library handle
+    "libOmxAacDec.so",
+    {
+      "audio_decoder.aac"
+    }
+  },
+  {
     "OMX.QCOM.CUST.COMP.START",
     NULL, //Create instance function
     //Unique instance handle
